fafc34727146ec39d35523b34b64127158a8191a
Marie Cadennes <mcadennes@asplus-brain.(none)>
2013-02-17 18:59:58 +0100

    add README and iodata.py

---
+++ ml/mulm_gpu/mulm/iodata.py
@@ -0,0 +1,25 @@
+#########
+## READ IN PYTHON
+#########
+
+import numpy as np
+
+def read_XYZresults(gen_filename, ima_filename, covar_filename,
+    permut_filename):
+    ### SNPs Data -------------------------------------------------------------
+    X = np.fromfile(gen_filename, dtype='int32', count=500 * 85772, \
+                    sep="").reshape(85772, 500).T
+    ### Imaging ---------------------------------------------------------------
+    fdi = open(ima_filename, mode='rb')
+    lines = fdi.readlines()
+    header_ima = lines[0]
+    data = lines[1:]
+    Y = np.asarray([[float(i) for i in l.split()] for l in data])
+    ### covariates ------------------------------------------------------------
+    fdi = open(covar_filename, mode='rb')
+    lines = fdi.readlines()
+    header_covar = lines[0]
+    data = lines[1:]
+    Z = np.asarray([[float(i) for i in l.split()] for l in data])
+
+    return X,Y,Z
