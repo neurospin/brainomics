e90cc0a6743f258b212c4cdda8eddb378b5bad86
Marie Cadennes <mcadennes@asplus-brain.(none)>
2013-04-17 12:29:33 +0200

    compilation and installation with cmake

---
+++ ml/mulm_gpu/mulm/include/mulm_utils.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include <sys/time.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define PREC_FLOAT	1.e-4
+#define tri_ind(i,j) (((i)>(j))?(((i)*((i)+1)>>1)+(j)):(((j)*((j)+1)>>1)+(i)))
+
+typedef	float	flottant;
+	   
+
