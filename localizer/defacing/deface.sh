703d7296993b7efb96ede7bbbb4e63f335bb876b
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2013-05-02 11:14:24 +0200

    Attempt to deface Localizer MRI T1 data
    Use the automated defacing tools of FreeSurfer, as documented here:
    http://www.na-mic.org/Wiki/index.php/Mbirn:_Defacer_for_structural_MRI

---
+++ localizer/defacing/deface.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SOURCE_DIR='/neurospin/brainomics/2012_brainomics_localizer/data/subjects'
+OUTPUT_DIR='/volatile/2012_brainomics_localizer/data/subjects'
+
+# configure FreeSurfer
+FREESURFER_HOME='/i2bm/local/freesurfer'
+export FREESURFER_HOME
+. ${FREESURFER_HOME}/FreeSurferEnv.sh
+
+# create output directory 
+mkdir -p "$OUTPUT_DIR"
+
+# iterate over source 'anat.nii.gz' T1 files
+for subject in "${SOURCE_DIR}/"*
+do
+    subject=`basename "$subject"`
+    # create output directory
+    echo mkdir -p "${OUTPUT_DIR}/${subject}/anat"
+    # output directory will contain 'anat_defaced.nii.gz' and log file
+    echo cd "${OUTPUT_DIR}/${subject}/anat"
+    echo mri_deface \
+        "${SOURCE_DIR}/${subject}/anat/anat.nii.gz" \
+        "${FREESURFER_HOME}/average/talairach_mixed_with_skull.gca" \
+        "${FREESURFER_HOME}/average/face.gca" \
+        'anat_defaced.nii.gz'
+done
