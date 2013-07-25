e7e7fb5529563293aaa0a3965ddb975c2b0f835d
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2013-07-25 08:53:18 +0200

    Deface both anat.nii.gz and raw_anat.nii.gz

--- localizer/anonymization/deface.sh
+++ localizer/anonymization/deface.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-SOURCE_DIR='/neurospin/brainomics/2012_brainomics_localizer/data/subjects'
-OUTPUT_DIR='/volatile/2012_brainomics_localizer/data/subjects'
+SOURCE_DIR='/neurospin/brainomics/2012_brainomics_localizer/data'
+OUTPUT_DIR='/volatile/2012_brainomics_localizer/data'
 
 # configure FreeSurfer
 FREESURFER_HOME='/i2bm/local/freesurfer'
 export FREESURFER_HOME
 . ${FREESURFER_HOME}/FreeSurferEnv.sh
 
-# iterate over source 'anat.nii.gz' T1 files
-for subject in "${SOURCE_DIR}/"*
+# process subjects
+for subject in "${SOURCE_DIR}/subjects/"*
 do
     subject=`basename "$subject"`
     # create output directory
-    mkdir -p "${OUTPUT_DIR}/${subject}/anat"
-    # output directory will contain 'anat_defaced.nii.gz' and log file
-    cd "${OUTPUT_DIR}/${subject}/anat"
+    mkdir -p "${OUTPUT_DIR}/subjects/${subject}/anat"
+    # log file will be written into current directory
+    cd "${OUTPUT_DIR}/subjects/${subject}/anat"
+    # anat.nii.gz -> anat_defaced.nii.gz
     mri_deface \
-        "${SOURCE_DIR}/${subject}/anat/anat.nii.gz" \
+        "${SOURCE_DIR}/subjects/${subject}/anat/anat.nii.gz" \
         "${FREESURFER_HOME}/average/talairach_mixed_with_skull.gca" \
         "${FREESURFER_HOME}/average/face.gca" \
         'anat_defaced.nii.gz'
+    # raw_anat.nii.gz -> raw_anat_defaced.nii.gz
+    mri_deface \
+        "${SOURCE_DIR}/subjects/${subject}/anat/raw_anat.nii.gz" \
+        "${FREESURFER_HOME}/average/talairach_mixed_with_skull.gca" \
+        "${FREESURFER_HOME}/average/face.gca" \
+        'raw_anat_defaced.nii.gz'
 done
