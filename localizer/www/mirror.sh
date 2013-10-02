eab8c32f73fab3498fa0d008760d36a19651aac7
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2013-10-02 15:02:32 +0200

    Static pages for Brainomics/Localizer
    These are Bootstrap-based static pages for the Brainomics/Localizer site.
    The mirror.sh script mirrors the pages to the test server - excluding itself.

---
+++ localizer/www/mirror.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#
+# local directory - the one this script lives in
+#
+SOURCE=`dirname "$0"`
+
+#
+# name of this script - to exclude it from mirroring
+#
+MYSELF=`basename "$0"`
+
+#
+# mirror local directory to /var/www on the test server
+#
+rsync -av -e 'ssh' \
+    --exclude="$MYSELF" \
+    "$SOURCE" \
+    root@neurospin-cubicweb.intra.cea.fr:/var/www
