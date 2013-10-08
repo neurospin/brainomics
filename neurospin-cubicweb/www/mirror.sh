ed9f61d61bb8ea74b5688073a3611add7cdbe19e
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2013-10-08 16:39:42 +0200

    Easier deployment on brainomics.cea.fr

--- neurospin-cubicweb/www/mirror.sh
+++ neurospin-cubicweb/www/mirror.sh
@@ -13,7 +13,7 @@ MYSELF=`basename "$0"`
 #
 # mirror local directory to /var/www on the test server
 #
-rsync -av -e 'ssh' \
+rsync -rlptv -e 'ssh' \
     --exclude="$MYSELF" \
     "$SOURCE" \
     root@neurospin-cubicweb.intra.cea.fr:/var/www
