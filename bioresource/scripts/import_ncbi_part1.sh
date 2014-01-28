878ba19117ca1f2cb174ef886944351b75dd6186
Jinpeng LI <mr.li.jinpeng@gmail.com>
2014-01-28 17:11:30 +0100

    ENH: add import scripts

---
+++ bioresource/scripts/import_ncbi_part1.sh
@@ -0,0 +1,5 @@
+# This script will be terminated very fast
+CUBENAME=bioresource
+INSTANCENAME=inst_$CUBENAME
+
+cubicweb-ctl shell $INSTANCENAME ./importncbi.py /volatile/jinpeng/frouin/ncbi/scripts/ncbi
