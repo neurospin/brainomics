bf9c2a546a1cb6ed96134477e47288c9629ba7cd
Jinpeng LI <mr.li.jinpeng@gmail.com>
2014-01-31 14:56:36 +0100

    ENH: add script to check the number of snps

---
+++ bioresource/scripts/check_snp_num.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 09:47:59 2014
+
+@author: jl237561
+"""
+
+import re
+
+
+def is_useful_chromosome(chromosome_name):
+    '''
+    >>> is_useful_chromosome(u'chr6_apd_hap1')
+    True
+    >>> is_useful_chromosome(u'chr9_gl000198_random')
+    False
+    >>> is_useful_chromosome(u'chrX')
+    True
+    >>> is_useful_chromosome(u'chr1')
+    True
+    >>> is_useful_chromosome(u'chr11')
+    True
+    >>> is_useful_chromosome(u'chrUn_gl000229')
+    False
+    >>> is_useful_chromosome(u'chr6_dbb_hap3')
+    True
+    >>> is_useful_chromosome(u'chrUn_gl000225')
+    False
+    '''
+    not_useful_patterns = [u"ch(.*)_random",
+                           u"chrUn_(.*)"]
+    for nu_pattern in not_useful_patterns:
+        res = re.search(nu_pattern, chromosome_name)
+        if res is not None:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    snps_data_path = "/neurospin/brainomics/2014_bioresource/data/snps/snp138Common.txt"
+    num_snps = 0
+    with open(snps_data_path) as infile:
+        for line in infile:
+            words = line.split("\t")
+            if len(words) > 0:
+                ch_name = unicode(words[1])
+                is_chr = is_useful_chromosome(ch_name)
+                if is_chr:
+                    num_snps = num_snps + 1
+    print num_snps
