28ef488a922efb3aa59d457bc6c2624f6cfad3b0
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2014-02-14 08:42:15 +0100

    Script will be run from imagen@imagen2 from now on
    Scritp will not be run from root@imagen2 anymore.

--- imagen/imagen2.cea.fr/disk/home/imagen/bin/mirror2sftp.sh
+++ imagen/imagen2.cea.fr/disk/home/imagen/bin/mirror2sftp.sh
@@ -6,7 +6,3 @@ rsync -r /neurospin/imagen/FU2/RAW/PSC2/QC /chroot/data/FU2/RAW/PSC2/
 
 # sftp_mirror
 rsync -r /neurospin/imagen/FU2/processed/sftp_mirror/ /chroot/data/FU2/processed
-
-# clean up
-chown -Rh imagen:imagen /chroot/data
-chmod -R g-w,o-w /chroot/data
