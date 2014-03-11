a3e5a83deacc3349dd7e313246b372bb1180f134
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2014-03-11 11:08:04 +0100

    Beautify

--- bioresource/examples/python/bioresourcesdb/__init__.py
+++ bioresource/examples/python/bioresourcesdb/__init__.py
@@ -1,40 +1,51 @@
-import sys, urllib2, urllib, json
+# -*- coding: utf-8 -*-
 
-class bioresourcesdb_base(object):
+import sys
+import urllib2
+import urllib
+import json
+
+
+class bioresourcedb_base(object):
     def studies(self):
         return [i[1] for i in self.rql('Any S,N  WHERE S is Study, S name N')]
 
 
-
-class bioresourcesdb_HTTP(bioresourcesdb_base):
-    url = 'http://is222241.intra.cea.fr:8080'
+class bioresourcesdb_HTTP(bioresourcedb_base):
+    _BIORESOURCE_DB_URL = 'http://is222241.intra.cea.fr:8080'
+    _EXPORT_TYPE = 'json'
 
     def __init__(self, login=None, password=None, url=None):
         if url is not None:
             self.url = url
+        else:
+            self.url = _BIORESOURCE_DB_URL
         if login:
             self.login(login, password)
             
-    def login( self, login=None, password=None ):
-        self.opener = urllib2.build_opener( urllib2.HTTPCookieProcessor() )
-        values = {
+    def login(self, login, password):
+        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+        data = {
             '__login': login,
             '__password': password,
-            }
-        self.opener.open( self.url, urllib.urlencode( values ) )
+        }
+        self.opener.open(self.url, urllib.urlencode(data))
 
     def entities(self, entity_type):
-        return json.load(self.opener.open( 
-                    '%s/%s?vid=ejsonexport' % (self.url, entity_type)))
+        data = {
+            'vid': 'e' + _EXPORT_TYPE + 'export',
+        }
+        return json.load(self.opener.open(self.url + '/' + entity_type,
+                                           urllib.urlencode(data)))
 
-    def rql( self, rql, data=None ):
+    def rql(self, rql, data=None):
         if data is not None:
-            rql = rql % data
-        values = {
+            rql %= data
+        data = {
             'rql': rql,
-            'vid': 'jsonexport',
-            }
-        return json.load( self.opener.open( self.url, urllib.urlencode( values ) ) )
+            'vid': _EXPORT_TYPE + 'export',
+        }
+        return json.load(self.opener.open(self.url, urllib.urlencode(data)))
 
 
 BioresourcesDB = bioresourcesdb_HTTP()
