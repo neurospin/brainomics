cf0014254fa18ea5b8a11da1bee5f717f2f1b21c
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2014-03-24 20:31:34 +0100

    Simplify file sync'ing
    Remove processed for now.

--- imagen/cron/mirror2sftp.sh
+++ imagen/cron/mirror2sftp.sh
@@ -20,7 +20,7 @@ ROOT_TARGET='/chroot/data'
 # FU2 - QC
 # needs no anonymization
 rsync -rlt \
-    /neurospin/imagen/FU2/RAW/PSC2/QC \
+    /neurospin/imagen/FU2/RAW/PSC2_FIXED/QC \
     ${ROOT_TARGET}/FU2/RAW/PSC2/
 
 
@@ -29,22 +29,6 @@ rsync -rlt \
 rsync -rlt \
     /neurospin/imagen/RAW/PSC2/psytools/*FU2*.csv \
     ${ROOT_TARGET}/FU2/RAW/PSC2/psytools/
-
-
-# FU2 - processed
-rsync -rlt \
-    /neurospin/imagen/FU2/processed/nifti \
-    /neurospin/imagen/FU2/processed/spmpreproc \
-    /neurospin/imagen/FU2/processed/spmstatsintra \
-    ${ROOT_TARGET}/FU2/processed/
-
-
-# FU2 - processed_ftp
-rsync -rlt \
-    /neurospin/imagen/FU2/processed_ftp/nifti \
-    /neurospin/imagen/FU2/processed_ftp/spmpreproc \
-    /neurospin/imagen/FU2/processed_ftp/spmstatsintra \
-    ${ROOT_TARGET}/FU2/processed/
 
 
 # clean up
